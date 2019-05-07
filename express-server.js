46d828422f662a313277f05fc879dbdc12f8c7be
Kayley <kayley.luftig@gmail.com>
2019-05-07 20:26:03 +0000

    Display JSON on /urls.json page

--- express-server.js
+++ express-server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 var PORT = 8080; //default port 8080
 
+//variable array with keys and values to display
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,6 +12,11 @@ var urlDatabase = {
 // handling get request for the root/index path "homepage"
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// calling object urlDatabase json page
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 //checking port and establishing localhost
